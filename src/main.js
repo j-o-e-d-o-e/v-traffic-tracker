8a081671d9e9e48a94567d890ca99470f83ccb2f
joedoe <d_joe@gmx.net>
2021-09-15 19:28:48 +0200

    bootstrap added

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Router from './router'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
 createApp(App).use(Router).mount('#app')
