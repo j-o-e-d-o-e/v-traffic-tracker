87fd07a43fbe3e940aa1f4153e7ae7068c22e311
joedoe <d_joe@gmx.net>
2021-09-17 13:13:13 +0200

    flights-line-chart added

---
+++ src/components/LineChart.vue
@@ -0,0 +1,34 @@
+<script>
+import {defineComponent} from 'vue'
+import {Line} from 'vue3-chart-v2'
+
+export default defineComponent({
+  name: 'Flights',
+  extends: Line,
+  props: {
+    chartData: Array,
+    chartLabels: Array
+  },
+  methods: {
+    labels() {
+      let chartLabels = [];
+      for (let i = 0; i < 24; i++) {
+        chartLabels.push(i.toString() + ':00');
+      }
+      return chartLabels;
+    }
+  },
+  mounted() {
+    this.renderChart({
+      labels: this.chartLabels,
+      datasets: [
+        {
+          label: 'Flights',
+          backgroundColor: '#f87979',
+          data: this.chartData
+        }
+      ]
+    })
+  }
+})
+</script>
--- src/views/Day.vue
+++ src/views/Day.vue
@@ -1,3 +1,62 @@
 <template>
-  <h1>Day</h1>
+  <div v-if="!loading">
+    <h1>{{ day.date }}</h1>
+    <Flights :chart-data="day.hours_flight" :chart-labels="labels()"></Flights>
+    <div class="table-responsive">
+      <table class="table table-striped table-bordered">
+        <thead class="table-dark">
+        <tr>
+          <th>Total</th>
+          <th>Avg altitude</th>
+          <th>Avg speed</th>
+          <th>Wind speed</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>{{ day.total }}</td>
+          <td>{{ day.avg_altitude }} m</td>
+          <td>{{ day.avg_speed }} km/h</td>
+          <td>{{ day.wind_speed }} km/h</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
+
+<script>
+import {toTitleDate} from "@/util/Utils";
+import Flights from "../components/LineChart"
+
+export default {
+  name: 'Day',
+  components: {
+    Flights
+  },
+  data: function () {
+    return {
+      day: Object,
+      loading: true
+    };
+  },
+  methods: {
+    fetch: async function () {
+      const res = await fetch('https://traffic-tracker.herokuapp.com/api/days/current');
+      return res.json();
+    },
+    labels() {
+      let chartLabels = [];
+      for (let i = 0; i < 24; i++) {
+        chartLabels.push(i.toString() + ':00');
+      }
+      return chartLabels;
+    }
+  },
+  async created() {
+    this.day = await this.fetch();
+    this.day.date = toTitleDate(this.day.date);
+    this.loading = false;
+  }
+}
+</script>
