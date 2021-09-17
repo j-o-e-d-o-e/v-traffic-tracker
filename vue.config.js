79d9585d9b880e4adf0b0cf5c5ef0f728945b3f2
joedoe <d_joe@gmx.net>
2021-09-18 00:09:21 +0200

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,19 @@
+// module.exports = {
+//     devServer: {
+//         proxy: {
+//             '^/api': {
+//                 target: 'https://traffic-tracker.herokuapp.com',
+//                 changeOrigin: true,
+//                 logLevel: 'debug',
+//                 pathRewrite: {'^/api': '/'}
+//             }
+//         }
+//     }
+// }
+// vue.config.js file to be place in the root of your repository
+
 module.exports = {
-    devServer: {
-        proxy: {
-            '^/api': {
-                target: 'https://traffic-tracker.herokuapp.com',
-                changeOrigin: true,
-                logLevel: 'debug',
-                pathRewrite: {'^/api': '/'}
-            }
-        }
-    }
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/my-project/'
+        : '/'
 }
